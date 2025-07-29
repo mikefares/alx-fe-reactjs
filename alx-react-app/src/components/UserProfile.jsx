16a9796c4f5b85177a56ca3630ba4fd8188f9649
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-07-29 19:18:26 +0000

    Imported User profile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ function UserProfile(props){
     return(
         <div>
             <h2>{props.name}</h2>
-            <p>{props.age}</p>
-            <p>{props.bio}</p>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
         </div>
     )
 }
