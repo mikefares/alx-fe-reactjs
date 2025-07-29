c5a5fd53820055741837a62d420f7dc862a32c7a
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-07-29 18:14:15 +0000

    Imported components

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <header>
-            <h1>My favourite cities</h1>
+            <h1>My Favorite Cities</h1>
         </header>
     )
 }
