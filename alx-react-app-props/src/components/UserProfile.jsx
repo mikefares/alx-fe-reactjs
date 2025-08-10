155d85c6e032095edc2a226e21b6225cfb099bef
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-08-10 23:47:10 +0000

    Exercise 3

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
 
-function UserProfile(props){
-    return(
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    )
+function UserProfile() {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
