0b6a9989660fe13c5991d754c5ceae318a6ca823
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-08-11 01:13:10 +0000

    Exercise 4

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-// import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Services from "./Services";
 import Contact from "./Contact";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
